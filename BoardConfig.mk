9ea5848d0c5b24a65bd643f649691239e3c8cc99
TRISF <trisfaizal@gmail.com>
2025-04-19 21:09:47 +0700

    miatoll: device: Use KomodoOS-specific libperfmgr sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,7 +164,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2023-05-01
 
 # Sepolicy
-include device/lineage/sepolicy/libperfmgr/sepolicy.mk
+include device/komodo/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
